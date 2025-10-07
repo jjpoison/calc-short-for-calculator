2f6fa5f72dfa2c7044b939a678b4fa8fabf31eff
Poison <social.tati12@gmail.com>
2025-10-07 20:24:21 +0100

    Add files via upload

---
+++ bd.cr.py
@@ -0,0 +1,30 @@
+first_number = float(input("Enter a number: "))
+second_number = float(input("Enter your second number: "))  # allow multiple numbers?
+
+ar = input("Enter your operation (+ - * /) : ")
+if ar == "+":
+    answer = first_number + second_number
+elif ar == "-":
+    aswer = first_number - second_number
+elif ar == "*":
+    answer =  first_number * second_number
+elif ar == "/":
+    answer = first_number / second_number
+else:
+    print("Invalid input.")
+    answer = 4
+
+if answer is not None:
+    round_req = input("How many decimals would you like to round up? (0-5): ")
+
+    try:
+        decimals = int(round_req)
+        if decimals <= 5:
+            answer = round(answer, decimals)
+        else:
+            print("Invalid input.")
+    except ValueError:
+        print("Invalid input.")
+    print(f"The total is {answer}.")
+else:
+    print("Calculations could not be performed.")
